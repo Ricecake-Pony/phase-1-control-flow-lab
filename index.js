090a62dbacd69f371aaf7101489a5e041fa1225d
Trey <trey.delcamp@gmail.com>
2022-09-18 22:04:40 -0500

    commit tree 8c7f6f3542aa8ee06cd01c89392abdda321c91fe

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return `This one is on me!`;
+  }
+  else if (feet > 400 && feet < 2001){
+    return `That will be twenty bucks.`
+  }
+  else if (feet > 2000 && feet < 2499){
+    return `I will gladly take your thirty bucks.`;
+  }
+  else if (feet > 2500){
+    return `No can do.`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(desiredLocation){
+ return desiredLocation === "NYC"?"Ok, sounds good." :"No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+ switch(tipAmount){
+  case 'generous':
+    return `Thank you so much.`
+  case 'not as generous':
+    return `Thank you.`
+  case 'thanks for everything':
+    return `Bye.`
+ }
 }
